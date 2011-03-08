ed0399846d8b4a6d443d2599dc00dfe3d2823fd8
Shaun Walbridge <shaun.walbridge@gmail.com>
2011-03-08 11:06:48 -0800

    improved approach, write output file explicitly

--- generate-lines.R
+++ generate-lines.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-require('rgdal')
+library('rgdal')
+
 # the original data they sent on is all in NAD27, may want to check if they're
 # measuring things in a different datum for consistency
 
@@ -11,19 +12,36 @@ setwd("~/d/education/frog-pathogen")
 
 lakes <- readOGR(dsn="data/seki/lakes/SEKI_lakes.shp", layer="SEKI_lakes")
 
+# filter the list out by those which have the 'HasFrogs' attribute
+frog.lakes <- lakes$LAKEID[!is.na(lakes$HasFrogs)]
+
 lines <- list()
 d <- data.frame(ID=character(0), from_id=numeric(0), to_id=numeric(0))
 
 # compute the full line matrix
-for (i in 1:nrow(lakes)) {
-  # get two consecutive points for calculating the line. To do the distance matrix, you'd need
-  # to compute the full m*n == (m*m-1)/2 set of lines.
-    
-  lines[[i]] <- Lines(Line(rbind(c(lakes$X_CENTER[1], lakes$Y_CENTER[1]), c(lakes$X_CENTER[i], lakes$Y_CENTER[i]))), ID = as.character(i))
-  d <- rbind(d, data.frame(ID=as.character(i), from_id=lakes$LAKEID[1], to_id=lakes$LAKEID[i+1]))
+computed <- c()
+k <- 1 
+# there's probably an existing function in R (perhaps using dist()), but this isn't an expensive
+# operation to compute manually, so doing it the ol' fashion way here. This generates the full
+# m*n matrix, for a total of (m*m-1) / 2 lines; here 195625.
+for (i in 1:length(frog.lakes)) {
+  source <- frog.lakes[i]
+  computed <- append(computed, source)
+  for (j in 1:length(frog.lakes)) {
+    dest <- frog.lakes[j]
+
+    # this is a candidate lake pairing: we've computed neither it or its inverse
+    if (!(dest %in% computed)) {
+      lines[[k]] <- Lines(Line(rbind(c(lakes$X_CENTER[i], lakes$Y_CENTER[i]), c(lakes$X_CENTER[j], lakes$Y_CENTER[j]))), ID = as.character(k)) 
+      d <- rbind(d, data.frame(ID=as.character(k), from_id=lakes$LAKEID[i], to_id=lakes$LAKEID[j]))
+      k <- k + 1
+      #cat(k, "\n")
+    }
+  }
 }
 
-sldf <- SpatialLinesDataFrame(SpatialLines(lines), d, match.ID = FALSE)
+# cast our lines into SpatialLines objects, copying in the CRS from the source dataset,
+# including our attributes data frame
+sldf <- SpatialLinesDataFrame(SpatialLines(lines, proj4string=CRS(proj4string(lakes))), d, match.ID = FALSE)
 
-# XXX SPECIFY SPATIAL REFERENCE
-writeOGR(sldf, "data/generated/single.shp", "single", driver="ESRI Shapefile")
+writeOGR(sldf, "data/generated/full.shp", "full", driver="ESRI Shapefile")
